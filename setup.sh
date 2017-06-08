22fff5a0f2c48df6e2093e087941c07bae2cfad3
Walter Carvalho <waltervascarvalho@gmail.com>
2017-06-08 15:14:09 +0100

    Fix setup.sh

--- setup.sh
+++ setup.sh
@@ -1,31 +1,8 @@
 #!/bin/bash
 date >> dotlog
-
-files=(
-    "config/.gitconfig"
-    "config/.zshrc"
-)
-
-function perform {
-    echo -ne "\t\t • $1"
-    eval $2 >> dotlog
-    echo -e " ✔\\r"
-}
-
 echo "Running setup..."
-
-echo -e "\t• Installing:"
-# perform homebrew "ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)""
-# perform nvm "curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.26.1/install.sh | bash"
-# perform rbenv "brew update && brew install rbenv ruby-build"
-
-# symlink dot files
-echo -e "\t• Linking:"
-for f in "${!files[@]}"
-    do
-        perform "${files["$f"]}" "echo ${files["$f"]}" # link files
-    done
-
-# clean
+ln -sf config/.gitconfig ~/.gitconfig
+ln -sf config/.zshrc ~/.zshrc
+ln -sf ~/code/dot/snippets ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/snippets
 echo "Setup complete."
 exit
